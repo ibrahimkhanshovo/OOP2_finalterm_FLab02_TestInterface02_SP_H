48a8c06e5a42b6e2455e3d10d91e834c926446d2
Ibrahim Khan Shovo <78006923+ibrahimkhanshovo@users.noreply.github.com>
2021-03-28 14:00:59 +0600

    Bank-Interface

---
+++ bank.cs
@@ -0,0 +1,17 @@
+﻿            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+
+namespace Bank_interface
+    {
+        interface Bank_IF
+        {
+            bool Deposit(decimal amount);
+            bool Withdraw(decimal amount);
+        }
+    }
+}
+    }
+}
